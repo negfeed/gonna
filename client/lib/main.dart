3b6b495f83bb49f24c7ef06d2fa38fe16a3b1d54
Mohammad Shamma <mohammadshamma@gmail.com>
2020-01-04 01:08:33 -0800

    Adds the layout to the create plan page.

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,8 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Gonna',
-      theme: ThemeData(
-          primarySwatch: Colors.blue),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: HomePage(title: 'Gonna'),
     );
   }
@@ -62,22 +62,105 @@ class CreatePlanPage extends StatefulWidget {
   _CreatePlanPageState createState() => _CreatePlanPageState();
 }
 
-class _CreatePlanPageState extends State<CreatePlanPage> {
+class _CreatePlanPageState extends State<CreatePlanPage>
+    with TickerProviderStateMixin {
+  Future<void> _showDatePicker() async {
+    await showDatePicker(
+        context: this.context,
+        initialDate: DateTime.now().add(Duration(days: 1)),
+        firstDate: DateTime.now(),
+        lastDate: DateTime.now().add(Duration(days: 7)));
+  }
+
+  Future<void> _showTimePicker() async {
+    await showTimePicker(context: this.context, initialTime: TimeOfDay.now());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        label: Text('Publish'),
+        icon: Icon(Icons.publish),
+      ),
       appBar: AppBar(
         title: Text('Create Plan'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Create Plan Page Body',
+      body: ListView(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Name',
+                hasFloatingPlaceholder: true,
+                border: InputBorder.none,
+              ),
             ),
-          ],
-        ),
+          ),
+          Divider(thickness: 1),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Date',
+                hasFloatingPlaceholder: true,
+                border: InputBorder.none,
+              ),
+              onTap: _showDatePicker,
+              readOnly: true,
+            ),
+          ),
+          Divider(thickness: 1),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Start Time',
+                hasFloatingPlaceholder: true,
+                border: InputBorder.none,
+              ),
+              onTap: _showTimePicker,
+              readOnly: true,
+            ),
+          ),
+          Divider(thickness: 1),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'End Time',
+                hasFloatingPlaceholder: true,
+                border: InputBorder.none,
+              ),
+              onTap: _showTimePicker,
+              readOnly: true,
+            ),
+          ),
+          Divider(thickness: 1),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
+            child: new Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+              Flexible(
+                fit: FlexFit.loose,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Extra Notes',
+                    hasFloatingPlaceholder: true,
+                    border: InputBorder.none,
+                  ),
+                  maxLines: null,
+                  minLines: null,
+                ),
+              ),
+            ]),
+          ),
+        ],
       ),
     );
   }
