3bc89db02985aa821c5e3edf05a115880209c001
Mohammad Shamma <mohammadshamma@gmail.com>
2020-01-07 01:47:30 -0800

    Adds field validation in the create event form.
    refs #2

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -65,33 +65,103 @@ class CreatePlanPage extends StatefulWidget {
 
 class _CreatePlanPageState extends State<CreatePlanPage>
     with TickerProviderStateMixin {
+  final _formKey = GlobalKey<FormState>();
+  final _dateController = TextEditingController();
+  final _timeController = TextEditingController();
+  final _durationController = TextEditingController();
+
+  final List<int> _durations = [1, 2, 3, 4, 5, 6, 7, 8];
+  final List<String> _durationStrings = [];
+
+  @override
+  void initState() {
+    super.initState();
+    for (var i = 0; i < _durations.length; i++) {
+      if (_durations[i] == 1) {
+        _durationStrings.add("${_durations[i]} hour");
+      } else {
+        _durationStrings.add("${_durations[i]} hours");
+      }
+    }
+  }
+
+  DateTime _date;
+  TimeOfDay _time;
+  Duration _duration;
+  int _durationPickedIndex;
+
+  String _nameValidator(String value) {
+    if (value.length == 0) {
+      return "Enter an event name.";
+    }
+    return null;
+  }
+
+  String _dateValidator(String value) {
+    if (value.length == 0) {
+      return "Choose a date.";
+    }
+    return null;
+  }
+
+  String _timeValidator(String value) {
+    if (value.length == 0) {
+      return "Choose a time.";
+    }
+    return null;
+  }
+
+  String _durationValidator(String value) {
+    if (value.length == 0) {
+      return "Choose duration.";
+    }
+    return null;
+  }
+
+  void _onPublishPlan() {
+    if (_formKey.currentState.validate()) {
+      print("Yay, validation passed!");
+    }
+  }
+
   Future<void> _showDatePicker() async {
-    await showDatePicker(
+    DateTime tomorrow = DateTime.now().add(Duration(days: 1));
+    DateTime initialDate = _date != null ? _date : tomorrow;
+    DateTime pickedDateTime = await showDatePicker(
         context: this.context,
-        initialDate: DateTime.now().add(Duration(days: 1)),
+        initialDate: initialDate,
         firstDate: DateTime.now(),
-        lastDate: DateTime.now().add(Duration(days: 10)));
+        lastDate: DateTime.now().add(Duration(days: 14)));
+    if (pickedDateTime != null) {
+      _date = pickedDateTime;
+      _dateController.text =
+          MaterialLocalizations.of(context).formatMediumDate(pickedDateTime);
+    }
   }
 
   Future<void> _showTimePicker() async {
-    await showTimePicker(context: this.context, initialTime: TimeOfDay.now());
+    TimeOfDay noon = TimeOfDay(hour: 12, minute: 0);
+    TimeOfDay initialTime = _time != null ? _time : noon;
+    TimeOfDay pickedTime =
+    await showTimePicker(context: this.context, initialTime: initialTime);
+    if (pickedTime != null) {
+      _time = pickedTime;
+      _timeController.text =
+          MaterialLocalizations.of(context).formatTimeOfDay(pickedTime);
+    }
   }
 
   Future<void> _showDurationPicker() async {
+    var pickedIndex = _durationPickedIndex != null ? _durationPickedIndex : 0;
     new Picker(
-        adapter: PickerDataAdapter<String>(pickerdata: [
-          "1 hour",
-          "2 hours",
-          "3 hours",
-          "4 hours",
-          "5 hours",
-          "6 hours",
-          "7 hours",
-          "8 hours"
-        ]),
+        selecteds: [pickedIndex],
+        adapter: PickerDataAdapter<String>(pickerdata: _durationStrings),
         hideHeader: true,
-        title: new Text("Please Select"),
+        title: new Text("Select Duration"),
         onConfirm: (Picker picker, List value) {
+          _durationPickedIndex = picker.selecteds[0];
+          _durationController.text = picker.getSelectedValues()[0];
+          _duration = Duration(hours: _durations[_durationPickedIndex]);
           print(value.toString());
           print(picker.getSelectedValues());
         }).showDialog(context);
@@ -101,86 +171,94 @@ class _CreatePlanPageState extends State<CreatePlanPage>
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          // Add your onPressed code here!
-        },
+        onPressed: _onPublishPlan,
         label: Text('Publish'),
         icon: Icon(Icons.publish),
       ),
       appBar: AppBar(
         title: Text('Create Plan'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: 'Name',
-                hasFloatingPlaceholder: true,
-                border: InputBorder.none,
-              ),
-            ),
-          ),
-          Divider(thickness: 1),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: 'Date',
-                hasFloatingPlaceholder: true,
-                border: InputBorder.none,
-              ),
-              onTap: _showDatePicker,
-              readOnly: true,
-            ),
-          ),
-          Divider(thickness: 1),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: 'Start Time',
-                hasFloatingPlaceholder: true,
-                border: InputBorder.none,
-              ),
-              onTap: _showTimePicker,
-              readOnly: true,
-            ),
-          ),
-          Divider(thickness: 1),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: 'Duration',
-                hasFloatingPlaceholder: true,
-                border: InputBorder.none,
-              ),
-              onTap: _showDurationPicker,
-              readOnly: true,
-            ),
-          ),
-          Divider(thickness: 1),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
-            child:
-                new Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-              Flexible(
-                fit: FlexFit.loose,
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Extra Notes',
-                    hasFloatingPlaceholder: true,
-                    border: InputBorder.none,
-                  ),
-                  maxLines: null,
-                  minLines: null,
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Name',
+                  hasFloatingPlaceholder: true,
+                  border: InputBorder.none,
                 ),
+                validator: _nameValidator,
               ),
-            ]),
-          ),
-        ],
+            ),
+            Divider(thickness: 1),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Date',
+                  hasFloatingPlaceholder: true,
+                  border: InputBorder.none,
+                ),
+                onTap: _showDatePicker,
+                readOnly: true,
+                controller: _dateController,
+                validator: _dateValidator,
+              ),
+            ),
+            Divider(thickness: 1),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Start Time',
+                  hasFloatingPlaceholder: true,
+                  border: InputBorder.none,
+                ),
+                onTap: _showTimePicker,
+                readOnly: true,
+                controller: _timeController,
+                validator: _timeValidator,
+              ),
+            ),
+            Divider(thickness: 1),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Duration',
+                  hasFloatingPlaceholder: true,
+                  border: InputBorder.none,
+                ),
+                onTap: _showDurationPicker,
+                readOnly: true,
+                controller: _durationController,
+                validator: _durationValidator,
+              ),
+            ),
+            Divider(thickness: 1),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
+              child:
+              new Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Extra Notes',
+                      hasFloatingPlaceholder: true,
+                      border: InputBorder.none,
+                    ),
+                    maxLines: null,
+                    minLines: null,
+                  ),
+                ),
+              ]),
+            ),
+          ],
+        ),
       ),
     );
   }
