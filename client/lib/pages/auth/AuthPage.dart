d0d4d0165b9dad5772bc8c9f5afc5f87c8f0f50c
Mohammad Shamma <mohammadshamma@gmail.com>
2020-02-02 18:24:03 -0800

    Makes the facebook login button match (somewhat) the Facebook style.
    Also added a placeholder for what should go on the login page.

--- client/lib/pages/auth/AuthPage.dart
+++ client/lib/pages/auth/AuthPage.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:gonna_client/services/auth/auth.dart';
 
 class AuthPage extends StatelessWidget {
-
   final AuthService _auth = AuthService();
 
   _signInFacebook() {
@@ -14,16 +14,24 @@ class AuthPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Authenticate"),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(40),
+              child: Image(
+                  image:
+                      AssetImage('assets/memes/wait-a-minute-who-are-you.jpg')),
+            ),
             RaisedButton(
+                color: Color.fromRGBO(66, 103, 178, 1),
                 onPressed: _signInFacebook,
-                child: const Text('Sign in with Facebook', style: TextStyle(fontSize: 20))),
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: const Text(
+                  'LOG IN WITH FACEBOOK',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                )),
           ],
         ),
       ),
