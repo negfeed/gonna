1dfb0d8abfc4ff587146b65a4e4b85c8bdb273de
Mohammad Shamma <mohammadshamma@gmail.com>
2021-01-10 05:23:21 -0800

    Add stubbed out pages for phone verification and profile initialization.

---
+++ client/lib/pages/auth/ProfileInitializationPage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ProfileInitializationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'ProfileInitializationPage',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
