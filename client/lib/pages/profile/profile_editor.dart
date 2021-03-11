1729cf0342ced0375ee25d55819893bef6467990
Mohammad Shamma <mohammadshamma@gmail.com>
2021-03-11 00:09:54 -0800

    Add validation for text fields in the profile editor.

--- client/lib/pages/profile/profile_editor.dart
+++ client/lib/pages/profile/profile_editor.dart
@@ -12,6 +12,7 @@ class ProfileEditorPage extends StatefulWidget {
 class _ProfileEditorPageState extends State<ProfileEditorPage> {
   File _image;
   final picker = ImagePicker();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -26,71 +27,80 @@ class _ProfileEditorPageState extends State<ProfileEditorPage> {
                 constraints: BoxConstraints(
                   minHeight: viewportConstraints.maxHeight,
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(40.0),
-                      child: Container(
-                        width: 200,
-                        height: 200,
-                        child: Stack(
-                          children: [
-                            CircleAvatar(
-                              radius: 100,
-                              child: _image == null
-                                  ? Text("Add a profile picture")
-                                  : null,
-                              backgroundImage:
-                              _image != null ? FileImage(_image) : null,
-                            ),
-                            Align(
-                              alignment: Alignment.bottomRight,
-                              child: RawMaterialButton(
-                                  onPressed: () {
-                                    _showImagePickOptionsDialog(context);
-                                  },
-                                  elevation: 2.0,
-                                  fillColor: Colors.white,
-                                  child: Icon(
-                                    Icons.edit,
-                                    size: 20.0,
-                                  ),
-                                  padding: EdgeInsets.all(15.0),
-                                  shape: CircleBorder(),
-                                  constraints: BoxConstraints(minWidth: 0)),
-                            ),
-                          ],
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(40.0),
+                        child: Container(
+                          width: 200,
+                          height: 200,
+                          child: Stack(
+                            children: [
+                              CircleAvatar(
+                                radius: 100,
+                                child: _image == null
+                                    ? Text("Add a profile picture")
+                                    : null,
+                                backgroundImage:
+                                _image != null ? FileImage(_image) : null,
+                              ),
+                              Align(
+                                alignment: Alignment.bottomRight,
+                                child: RawMaterialButton(
+                                    onPressed: () {
+                                      _showImagePickOptionsDialog(context);
+                                    },
+                                    elevation: 2.0,
+                                    fillColor: Colors.white,
+                                    child: Icon(
+                                      Icons.edit,
+                                      size: 20.0,
+                                    ),
+                                    padding: EdgeInsets.all(15.0),
+                                    shape: CircleBorder(),
+                                    constraints: BoxConstraints(minWidth: 0)),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 40),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Add a First Name',
+                      Container(
+                        height: 50,
+                        padding: const EdgeInsets.symmetric(horizontal: 40),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'First Name',
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                          ),
+                          textCapitalization: TextCapitalization.sentences,
+                          validator: _validateFirstName,
                         ),
-                        textCapitalization: TextCapitalization.sentences,
                       ),
-                    ),
-                    Padding(padding: const EdgeInsets.symmetric(vertical: 10)),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 40),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Add a Last Name',
+                      Padding(padding: const EdgeInsets.symmetric(vertical: 10)),
+                      Container(
+                        height: 50,
+                        padding: const EdgeInsets.symmetric(horizontal: 40),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Last Name',
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                          ),
+                          textCapitalization: TextCapitalization.sentences,
+                          validator: _validateLastName,
                         ),
-                        textCapitalization: TextCapitalization.sentences,
                       ),
-                    ),
-                    Padding(padding: const EdgeInsets.symmetric(vertical: 20)),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Create Profile'),
-                      style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.all(20.0)),
-                    ),
-                  ],
+                      Padding(padding: const EdgeInsets.symmetric(vertical: 20)),
+                      ElevatedButton(
+                        onPressed: _onCreateProfile,
+                        child: const Text('Create Profile'),
+                        style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.all(20.0)),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             );
@@ -151,5 +161,29 @@ class _ProfileEditorPageState extends State<ProfileEditorPage> {
             ),
           ),
     );
+  }
+
+  String _validateFirstName(String value) {
+    if (value.length == 0) {
+      return "Enter your first name.";
+    }
+    return null;
+  }
+
+  String _validateLastName(String value) {
+    if (value.length == 0) {
+      return "Enter your last name.";
+    }
+    return null;
+  }
+
+  void _onCreateProfile() {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+    if (_image == null) {
+      // TODO: Add avatar editor error indicator.
+    }
+    print("Yay, validation passed!");
   }
 }
