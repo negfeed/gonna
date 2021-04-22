fc36ff8bc177f44a0cd2a12b86f11247a264f9fc
Mohammad Shamma <mohammadshamma@gmail.com>
2021-04-22 01:24:46 -0700

    Add a catch all UserVisibleError.
    This error class can be used whenever a service throws a
    non-UserVisibleError. This way the UI can handle all types
    of exceptions consistently.

--- client/lib/services/error.dart
+++ client/lib/services/error.dart
@@ -10,3 +10,16 @@ abstract class UserVisibleError implements Exception {
     return _cause;
   }
 }
+
+// This error class is meant to replace all non-UserVisibleErrors that are thrown
+// from services back to widgets. This way the widgets can consistently handle
+// errors thrown on service function calls.
+class CatchAllError extends UserVisibleError {
+  CatchAllError(Exception cause) : super(cause);
+
+  @override
+  String get longMessage => 'Oops, something went wrong. Please try again later.';
+
+  @override
+  String get shortMessage => 'Please try again later.';
+}
